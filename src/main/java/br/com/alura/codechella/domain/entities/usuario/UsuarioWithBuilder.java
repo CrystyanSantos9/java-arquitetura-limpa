7be1d96ffc42453b9dc628e8aef83bad76b8dbce
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2024-06-11 11:51:59 -0300

    testando fábrica e builder de usuario

---
+++ src/main/java/br/com/alura/codechella/domain/entities/usuario/UsuarioWithBuilder.java
@@ -0,0 +1,124 @@
+package br.com.alura.codechella.domain.entities.usuario;
+
+import br.com.alura.codechella.domain.Endereco;
+
+import java.time.LocalDate;
+
+public class UsuarioWithBuilder {
+
+    private Long id;
+    private String cpf;
+    private String nome;
+    private LocalDate nascimento;
+    private String email;
+    private Endereco endereco;
+
+    public UsuarioWithBuilder(UsuarioBuilder builder) {
+
+        if(builder.cpf == null || !builder.cpf.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")){
+            throw new IllegalArgumentException("CPF no padrão incorreto!");
+        }
+
+        this.id = builder.id;
+        this.cpf = builder.cpf;
+        this.nome = builder.nome;
+        this.nascimento = builder.nascimento;
+        this.email = builder.email;
+        this.endereco = builder.endereco;
+    }
+
+    public static class UsuarioBuilder {
+        private Long id;
+        private String cpf;
+        private String nome;
+        private LocalDate nascimento;
+        private String email;
+        private Endereco endereco;
+
+        public UsuarioBuilder id(Long id){
+            this.id = id;
+            return this;
+        }
+
+        public UsuarioBuilder id(String cpf){
+            this.cpf = cpf;
+            return this;
+        }
+
+        public UsuarioBuilder nome(String nome){
+            this.nome = nome;
+            return this;
+        }
+
+        public UsuarioBuilder nascimento(LocalDate nascimento){
+            this.nascimento = nascimento;
+            return this;
+        }
+
+        public UsuarioBuilder email(String email){
+            this.email = email;
+            return this;
+        }
+
+        public UsuarioBuilder endereco(Endereco endereco){
+            this.endereco = endereco;
+            return this;
+        }
+
+
+        public UsuarioWithBuilder build(){
+            return new UsuarioWithBuilder (this);
+        }
+
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
--- src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
+++ src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
@@ -1,5 +1,6 @@
 package br.com.alura.codechella.domain.entities.usuario;
 
+import br.com.alura.codechella.domain.Endereco;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +14,41 @@ class UsuarioTest {
                 ()-> new Usuario
                         (1L, "123.45.789-99", "Henrique", LocalDate.parse ("1990-09-08"),"hrq@gmail.com"));
     }
+
+    @Test
+    public void naoDeveCadastrarUsuarioComCpfNoFormatoInvalidoComBuilder(){
+
+        Assertions.assertThrows (IllegalArgumentException.class,
+                ()-> new UsuarioWithBuilder
+                        .UsuarioBuilder()
+                        .id (1L)
+                        .nascimento (LocalDate.parse ("1990-09-08"))
+                        .endereco (new Endereco ("2277-052", 195, "Rua da Pedra Ap 101"))
+                        .build ());
+    }
+
+    @Test
+    public void deveCriarUsuarioUsandoFabrica(){
+
+
+// A fabrica mantem a mesma instancia de usuario;
+
+        FabricaDeUsuario fabricaDeUsuario = new FabricaDeUsuario ();
+
+        Usuario usuario = fabricaDeUsuario
+                .comNomeCpfNascimento ("Crystyan", "133.287.148-34", LocalDate.parse ("1990-09-08") );
+
+        Assertions.assertEquals ("Crystyan", usuario.getNome ());
+
+// Por manter a mesma instancia, devolvemos o mesmo objeto com dados adicionais
+
+        usuario =fabricaDeUsuario.incluiEndereco ("2277-052", 195, "Rua da Pedra Ap 101");
+
+
+        Assertions.assertEquals (195, usuario.getEndereco ().getNumero ());
+        Assertions.assertEquals ("Rua da Pedra Ap 101", usuario.getEndereco ().getComplemento ());
+    }
+
+
 
 }
